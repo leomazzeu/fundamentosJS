d0c0877742d2cbf8308735d022691cea9150e5fc
Leonardo Mazzeu <leonardomazzeudasilva@gmail.com>
2021-01-26 20:19:58 -0300

    Primeiro exercício

---
+++ exercicios-JS/ex-01/script.js
@@ -0,0 +1,41 @@
+let weight
+console.log(typeof weight)
+
+//===========
+
+let name = 'Leonardo'
+let age = 21
+let stars = 65.9
+let isSubscribed = true
+
+//===========
+
+let student = {
+    name: 'Leonardo',
+    age: 21,
+    weight: 65.9,
+    isSubscribed: true
+};
+
+console.log(`${student.name} de idade ${student.age} pesa ${student.weight}kg`)
+
+//==================
+
+let students = []
+students = [student]
+console.log(students)
+console.log(students[0])
+
+//===================
+
+const john = {
+    name: 'John',
+    age: 24,
+    weight: 90.5,
+    isSubscribed: false
+}
+
+students.push(john)
+console.log(students)
+
+console.log(a)
