c314b53cde5dc855c1735c793fd39d0aa438c3b5
Leonardo Mazzeu <leonardomazzeudasilva@gmail.com>
2021-01-26 20:52:16 -0300

    Functions

---
+++ function/function-rocketseat/functions/functions.js
@@ -0,0 +1,12 @@
+// Criar um app de frases motivacionais
+
+
+// function statement
+function createPhrases() {
+    console.log('Estudar é muito bom!')
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mãe do aprendizado')
+}
+
+// call function
+console.log(createPhrases())
